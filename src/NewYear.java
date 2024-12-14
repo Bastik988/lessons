c8adef2c9d67076eea94af0c65670ca4caaccfca
Anastasia <anastasiathef@gmail.com>
2024-12-14 22:34:53 +0300

    Хз, можно ли на русском коммиты создавать, поэтому если будет набор символов левых, то не серчайте!) (мне лень переводчиком пользоваться...) Тут идёт получение даты пользователя:)

--- src/NewYear.java
+++ src/NewYear.java
@@ -1,7 +1,12 @@
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+import java.util.Scanner;
 public class NewYear {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss.SS");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¬ведите вашу дату и врем€");
+        String dateOne = sc.nextLine();
+        LocalDateTime dateTime = LocalDateTime.parse(dateOne, formatter);
     }
 }
